b10be66d91534ee756335fa0df4ca1b8c79dd3f8
Jesse Gajda <jessegajda@hotmail.com>
2020-10-08 18:38:05 -0400

    Add project files.

---
+++ SSDAssignment1/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SSDAssignment1.Models;
+
+namespace SSDAssignment1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
---
+++ SSDAssignment1/Data/Migrations/20200924221853_PleaseWork.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SSDAssignment1.Data.Migrations
+{
+    public partial class PleaseWork : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
